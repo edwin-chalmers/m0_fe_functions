ceb4a09915c49ebe8fb33b7f69897465d2b58cc1
Edwin Chalmers <edwinchalmers@gmail.com>
2023-11-10 03:26:48 -0700

    Complete the cleanUp exercise

--- cleanUp.js
+++ cleanUp.js
@@ -8,37 +8,52 @@
 
 
 // EX 1:
-function askForName() { console.log("Hello, what is your name?") }
+function askForName() { 
+  console.log("Hello, what is your name?") 
+}
 
 askForName()
 
+// I organized the code so that each opperation had its own line.
 
 // EX 2:
-function addThreeNums(first, second, third) {
-var sum = first + second + third
-return sum;
+function addThreeNums(num1, num2, num3) {
+var sum = num1 + num2 + num3;
+console.log(sum);
       }
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
 
+// I changed the name of the variables so that their purpose is more clearly understood.
+// I changed return to console.log so that the function would show an output.
+
 
 // EX 3:
-func makeFreshPesto(){
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");    
+}
 
 makeFreshPesto();
 
+// Function was spelled wrong
+// Adjusted the } so it would be on its own line
 
 //  EX 4:
-function average(num1, num2) 
-  {
+function average(num1, num2) {
 var sum = num1 + num2;
     var avg = sum / 2;
-
-return avg
+console.log(avg)
   }
+
+ average(7, 7)  
+ average(9, 21)
+
+// Adjusted { to be on the same line as "function"
+// changed return to console.log to get an output
+// removed spaces between var avg and console.log
+// added imput to the finction
